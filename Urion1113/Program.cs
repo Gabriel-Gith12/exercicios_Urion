21c428538b6dde681100f299aa7117beeaa99cf0
Gabriel-Gith12 <86987936+Gabriel-Gith12@users.noreply.github.com>
2021-07-07 17:47:20 -0300

    Add files via upload

---
+++ Urion1113/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Urion_1113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        /// Leia um número indeterminado de pares de valores inteiros. Escreva uma mensagem para cada par indicando se esses dois números estão em ordem crescente ou decrescente.
+
+        /// Entrada
+        /// O arquivo de entrada contém vários casos de teste. Cada caso de teste contém dois números inteiros X e Y. A entrada terminará quando X = Y.
+
+        /// Resultado
+        /// Para cada caso de teste imprima “Crescente”, se os valores X e Y estão em ordem crescente, caso contrário imprima “Decrescente”.
+
+        /// Variaveis
+        string[] vet;
+        int X, Y;
+
+        /// Entrada
+        vet = Console.ReadLine().Split(' ');
+        X = int.Parse(vet[0]);
+        Y = int.Parse(vet[1]);
+
+            while(X != Y)
+            {
+                if(X < Y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                vet = Console.ReadLine().Split(' ');
+                X = int.Parse(vet[0]);
+                Y = int.Parse(vet[1]);
+            }
+        }
+    }
+}
---
+++ Urion2046/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Urion_2046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        /// Leia a hora de início e de término de um jogo, em horas. Em seguida, calcule a duração do jogo, sabendo que o jogo pode começar em um dia e terminar
+        /// em outro, com duração máxima de 24 horas. A mensagem deve ser impressa em português “O JOGO DUROU X HORA (S)” que significa “O JOGO DURADO X HORA (S)”
+
+        /// Entrada
+        /// Dois números inteiros que representam a hora de início e de término de um jogo.
+
+        /// Resultado
+        /// Imprima a duração do jogo como na saída de amostra.
+
+        int início , término;
+        string[] vet;
+
+        vet = Console.ReadLine().Split(' ');
+        início = int.Parse(vet[0]);
+        término = int.Parse(vet[1]);
+
+
+
+
+        }
+    }
+}
