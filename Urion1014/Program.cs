0d1755fc65202e2117550a800f4406cad92a0896
Gabriel-Gith12 <86987936+Gabriel-Gith12@users.noreply.github.com>
2021-08-09 16:46:06 -0300

    Add files via upload

---
+++ Urion1014/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Urion_1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*            
+            Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).
+
+            Entrada
+            O arquivo de entrada contém dois valores: um valor inteiro X representando a distância total percorrida (em Km), e um valor real Y representando o total de combustível gasto, com um dígito após o ponto decimal.
+
+            Saída
+            Apresente o valor que representa o consumo médio do automóvel com 3 casas após a vírgula, seguido da mensagem "km/l".
+            */
+            double kml;
+
+            int X = int.Parse(Console.ReadLine());
+            double Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            kml = X / Y;
+
+            Console.WriteLine(kml.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
---
+++ Urion1036/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Urion1036
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, mostre a mensagem correspondente 
+            “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
+
+            Entrada
+            Leia três valores de ponto flutuante (double) A, B e C.
+
+            Saída
+            Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". Caso contrário, imprima o resultado das raízes com 5 dígitos após o 
+            ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.        
+            */
+
+            String[] vet = Console.ReadLine().Split(' ');
+            double A = Convert.ToDouble(vet[0]);
+            double B = Convert.ToDouble(vet[1]);
+            double C = Convert.ToDouble(vet[2]);
+
+            if ((B * B - 4 * A * C) >= 0 && (2 * A) != 0)
+            {
+                double x1 = (-B + Math.Sqrt(Math.Pow(B, 2) - 4 * A * C)) / (2 * A);
+
+                double x2 = (-B - Math.Sqrt(Math.Pow(B, 2) - 4 * A * C)) / (2 * A);
+
+                Console.WriteLine("R1 = {0:0.00000}", x1);
+
+                Console.WriteLine("R2 = {0:0.00000}", x2);
+            }
+            else
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+        }
+    }
+}
