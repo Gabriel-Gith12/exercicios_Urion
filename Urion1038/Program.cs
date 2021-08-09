13130ab3e45d03e2ad6ec1392e0bceca3a088e46
Gabriel Camargo <86987936+Gabriel-Gith12@users.noreply.github.com>
2021-08-09 17:05:17 -0300

    Add files via upload

---
+++ Urion1038/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Urion1038
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
+
+            Entrada
+            O arquivo de entrada contém dois valores inteiros correspondentes ao código e à quantidade de um item conforme tabela acima.
+
+            Saída
+            O arquivo de saída deve conter a mensagem "Total: R$ " seguido pelo valor a ser pago, com 2 casas após o ponto decimal.
+            */
+
+            double total;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            int codigo = int.Parse(vet[0]);
+            int quantidade = int.Parse(vet[1]);
+
+            if (codigo == 1)
+            {
+                total = quantidade * 4.0;
+            }
+            else if (codigo == 2)
+            {
+                total = quantidade * 4.5;
+            }
+            else if (codigo == 3)
+            {
+                total = quantidade * 5.0;
+            }
+            else if (codigo == 4)
+            {
+                total = quantidade * 2.0;
+            }
+            else
+            {
+                total = quantidade * 1.5;
+            }
+            Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
