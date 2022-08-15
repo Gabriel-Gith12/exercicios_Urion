bda98499bd836cd77c61718855dcbbe20f2cbefd
Gabriel Camargo <gabriel.camargo421@gmail.com>
2022-08-15 12:51:19 -0300

    Add files via upload

--- Urion1047/Program.cs
+++ Urion1047/Program.cs
@@ -6,15 +6,15 @@ namespace Urion1047
     {
         static void Main(string[] args)
         {
-            /*Leia a hora inicial, minuto inicial, hora final e minuto final de um jogo. A seguir calcule a duração do jogo.
-
+            /*Leia a hora inicial, minuto inicial, hora final e minuto final de um jogo. A seguir 
+            calcule a duração do jogo.
             Obs: O jogo tem duração mínima de um (1) minuto e duração máxima de 24 horas.
 
             Entrada
             Quatro números inteiros representando a hora de início e fim do jogo.
 
             Saída
-            Mostre a seguinte mensagem: “O JOGO DUROU XXX HORA(S) E YYY MINUTO(S)”.*/
+            Mostre a seguinte mensagem: “O JOGO DUROU XXX HORA(S) E YYY MINUTO(S)” .*/
 
             string[] valores = Console.ReadLine().Split(' ');
 
---
+++ Urion1060/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+
+namespace Urion1060
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Faça um programa que leia 6 valores. Estes valores serão somente negativos ou 
+            positivos (desconsidere os valores nulos). A seguir, mostre a quantidade de valores 
+            positivos digitados.
+
+            Entrada
+            Seis valores, negativos e/ou positivos.
+
+            Saída
+            Imprima uma mensagem dizendo quantos valores positivos foram lidos.*/
+
+            double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double d = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double e = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double f = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            int valorPositivo = 0;
+
+            if (a > 0.0)
+            {
+                valorPositivo += 1;
+            }
+
+            if (b > 0.0)
+            {
+                valorPositivo += 1;
+            }
+
+            if (c > 0.0)
+            {
+                valorPositivo += 1;
+            }
+
+            if (d > 0.0)
+            {
+                valorPositivo += 1;
+            }
+
+            if (e > 0.0)
+            {
+                valorPositivo += 1;
+            }
+
+            if (f > 0.0)
+            {
+                valorPositivo += 1;
+            }
+
+            Console.WriteLine(valorPositivo + " valores positivos");
+        }
+    }
+}
