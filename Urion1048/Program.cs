abbf564fe8c09260b9ca71a798d30537e09da205
Gabriel Camargo <gabriel.camargo421@gmail.com>
2022-08-15 12:36:16 -0300

    Update Program.cs

--- Urion1048/Program.cs
+++ Urion1048/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Globalization;
 
 namespace Urion1048
 {
@@ -24,6 +25,37 @@ namespace Urion1048
         /// Novo salario:  significa "Novo Salário "
         /// Reajuste ganho: significa "Dinheiro ganho"
         /// Em percentual: significa "Em percentagem"/
+             double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double percentual;
+
+            if (salario <= 400.0)
+            {
+                percentual = 15.0;
+            }
+            else if (salario <= 800.0)
+            {
+                percentual = 12.0;
+            }
+            else if (salario <= 1200.0)
+            {
+                percentual = 10.0;
+            }
+            else if (salario <= 2000.0)
+            {
+                percentual = 7.0;
+            }
+            else
+            {
+                percentual = 4.0;
+            }
+
+            double reajuste = salario * percentual / 100.0;
+            double novoSalario = salario + reajuste;
+            ///                Novo salario:       460.00
+            Console.WriteLine("Novo salario: " + novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: " + percentual.ToString("F0", CultureInfo.InvariantCulture) + " %");
         }
     }
 }
